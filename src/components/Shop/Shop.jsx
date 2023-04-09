44bf435d430498779dd005f75c9495a206c098c0
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-04-09 14:19:35 +0600

    added shop item

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CartItem from '../Cards/CartItem';
+import ProductCard from '../Cards/ProductCard';
 
 const Shop = () => {
-    const data = useLoaderData();
-    console.log(data)
+    const products = useLoaderData();
+    console.log(products)
     return (
-        <div>
-            <h3>Shop Page</h3>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-container gap-10  '>
+            {
+                products.map(product=><ProductCard product={product} key={product.id}></ProductCard>)
+            }
         </div>
     );
 };
